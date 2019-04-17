7ae792fac10b65952b1c5f36516195beca96e410
Cloud User <centos@ip-10-0-0-151.eu-west-1.compute.internal>
2019-04-17 15:32:38 +0000

    mod index custom init

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta name="author" content="">
     <link rel="icon" href="favicon.ico">
 
-    <title>APPUiO PHP Demo</title>
+    <title>APPUiO PHP Demo... Forked By me ... Fabio!:wq</title>
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
